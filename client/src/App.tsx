78ea0192b558e88253e5723a3fee4a0b0beb810c
Andor Kovács <andor.kovacs@ibm.com>
2021-08-10 12:00:50 +0200

    Fetch and list trucks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ILocation {
+  readonly latitude: number;
+  readonly longitude: number;
+  readonly state: string;
+  readonly city: string;
+  readonly zip?: undefined;
 }
 
-export default App;
+interface ITruck {
+  readonly id: string;
+  readonly location: ILocation;
+  readonly active: boolean;
+}
+
+export function App(): JSX.Element {
+
+  const [trucks, setTrucks] = useState<ITruck[] | null>(null)
+  const [selectedId, setSelectedId] = useState<string | null>(null)
+
+  useEffect(
+    (): void => {
+
+      async function getTrucks(): Promise<void> {
+
+        const response: AxiosResponse<ITruck[]> = await axios.get<ITruck[]>('https://s3.amazonaws.com/interview.aifleet.dev/trucks.json')
+
+        setTrucks(response.data)
+      }
+
+      if (!trucks) {
+        getTrucks()
+      }
+    },
+    [trucks]
+  )
+
+  function getLocationOfActiveTrucks(): string[] {
+
+    return trucks?.filter((truck: ITruck): boolean => truck.active).map((truck: ITruck): string => formatLocation(truck)) || []
+  }
+
+  function formatLocation(truck: ITruck): string {
+
+    return `${truck.location.city}${truck.location.zip ? ` ${truck.location.zip}` : ''}, ${truck.location.state}`
+  }
+
+  function getSelected(): ITruck | undefined {
+
+    return trucks?.find((truck: ITruck): boolean => truck.id === selectedId)
+  }
+
+  function getSelectedString(): string {
+
+    const selected: ITruck | undefined = getSelected()
+
+    return `Selected truck's coordinates: ${selected?.location.latitude}, ${selected?.location.longitude}`
+  }
+
+  function onSelect(id: string): (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => void {
+
+    return (event: React.MouseEvent<HTMLLIElement, MouseEvent>): void => {
+      setSelectedId(id)
+    }
+  }
+
+  return (
+    <>
+      {
+        !!trucks && !!trucks.length
+          ?
+          <>
+            <h1>List of the truck IDs</h1>
+            <ul>
+              {trucks.map((truck: ITruck): JSX.Element =>
+                <li key={truck.id} onClick={onSelect(truck.id)}>
+                  {truck.id}
+                </li>
+              )}
+            </ul>
+            {!!selectedId && <h2>{getSelectedString()}</h2>}
+            <h2>Locations</h2>
+            <ul>
+              {getLocationOfActiveTrucks().map((locationString: string): JSX.Element =>
+                <li key={locationString} >
+                  {locationString}
+                </li>
+              )}
+            </ul>
+          </>
+          :
+          'Loading...'
+      }
+    </>
+  );
+}
